ce0abd6913ae0eaafb42a193b1684aa7fd65f3d6
Johannes Schäfer <mail@johannes-schaefer.net>
2016-05-18 17:02:33 +0200

    Automatically create default admin user if none exists

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,9 +9,43 @@
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
+var randomString = require('randomstring');
+
 module.exports.bootstrap = function(cb) {
+
+  User.findOne({ status: 'admin' }).exec(function (err, user) {
+
+    if (!err && user) {
+      sails.log.debug('At least one user with status "admin" exists.');
+      return;
+    }
+
+    sails.log.warn('No user with status "admin" exists.');
+
+    var email = 'admin@beispiel.de'
+      , password = randomString.generate(12);
+
+    User.create({
+      email:    email,
+      password: password
+    }).exec(function (err, user) {
+
+      if( err ) {
+        sails.log.error('Error creating default admin user.');
+        sails.log.error(err);
+      } else {
+        sails.log.warn('Default admin user created.');
+        sails.log.warn('Email:    ' + user.email);
+        sails.log.warn('Password: ' + password + "\n");
+        sails.log.warn('Please change email and password of this admin asap!');
+      }
+
+    });
+
+  });
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
+
 };
